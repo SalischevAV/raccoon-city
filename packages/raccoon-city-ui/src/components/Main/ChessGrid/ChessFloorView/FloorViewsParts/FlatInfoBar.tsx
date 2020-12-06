91294b98650fdd7f13afca7afdd7f475602e9092
Alexey <hockslide@gmail.com>
2020-12-06 12:16:51 +0000

    Squashed commit of the following
    commit fa124827 
    Author: letrylesha <lunarboxnote@gmail.com> 
    Date: Sat Dec 05 2020 20:08:45 GMT+0300 (Москва, стандартное время) 
    
        fix price

--- packages/raccoon-city-ui/src/components/Main/ChessGrid/ChessFloorView/FloorViewsParts/FlatInfoBar.tsx
+++ packages/raccoon-city-ui/src/components/Main/ChessGrid/ChessFloorView/FloorViewsParts/FlatInfoBar.tsx
@@ -6,13 +6,26 @@ interface Props {
     info: any;
 }
 
+function getPrice(flat) {
+    const {squarePrice, squarePriceSale, area} = flat;
+
+    if (!squarePrice) {
+        return null;
+    }
+
+    const square = squarePriceSale ? squarePriceSale : squarePrice;
+    const price = area * square;
+
+    return price.toFixed(2);
+}
+
 export const FlatInfoBar = ({info}: Props) => (
     <FlatInfo>
         {info ? (
             <>
                 <span>{`№${info.flatNumber}`}</span>
                 <span>{`Статус: ${FLAT_STATUSES.find((statuses) => statuses.value === info.status)?.label}`}</span>
-                {info.status !== 'SOLD_OUT' && info.price && <span>{`Цена: ${info.price}`}</span>}
+                {info.status !== 'SOLD_OUT' && info.price && <span>{`Цена: ${getPrice(info)}`}</span>}
                 <span>{`М2: ${info.area}`}</span>
                 {(info.squarePriceSale || info.squarePrice) && (
                     <span>{`Цена м2: ${info.squarePriceSale || info.squarePrice}`}</span>
