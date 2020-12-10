6c73e2d1fbc6e4e86cdc57431991f4f5d7c0315b
Eugene Kovaljov <v-ykovalov@vrbo.com>
2020-12-10 18:25:37 +0200

    add log message

--- packages/raccoon-city-graphql/src/resolvers/mutations/trade.ts
+++ packages/raccoon-city-graphql/src/resolvers/mutations/trade.ts
@@ -7,6 +7,7 @@ import {ApolloError} from 'apollo-server';
 
 async function sendUserToAmo(url: string, user: any) {
     try {
+        logger.info(`sending flat request to ${url}`)
         const response = await axios.post(url, user);
         return response.status;
     } catch (err) {
